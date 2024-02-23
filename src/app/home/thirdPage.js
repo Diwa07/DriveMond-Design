2130e837c81d3603819db24896874b0c8b635197
Diwakar <khatiwadaddiwakar07@gmail.com>
2024-02-23 14:41:38 +0545

    solved3

--- src/app/home/thirdPage.js
+++ src/app/home/thirdPage.js
@@ -15,10 +15,10 @@ const ThirdPage = () => {
       </h1>
       <div className="h-auto p-4  mt-[2rem] w-full ">
         <div className="px-[7%]   ">
-          <div className="grid gap-[2rem] grid-cols-2">
-            <ul className="grid grid-cols-5 gap-4 text-xl  py-14 bg-[#EAFBF9] p-7 rounded-2xl  text-[#003F37] font-bold text-center ">
+          <div className="grid gap-[2rem]   grid-cols-1 lg:grid-cols-2">
+            <ul className="grid md:grid-cols-5  grid-cols-3 gap-4 text-[1.2rem]   py-10 bg-[#EAFBF9]  px-4 rounded-2xl  text-[#003F37] font-bold text-center ">
               <li>
-                <img src={php} alt="" className="w-full py-2 " />
+                <img src={php} alt="" className="w-full py-2 bg-white " />
                 PHP
               </li>
               <li>
@@ -38,10 +38,14 @@ const ThirdPage = () => {
                 Map Api
               </li>
             </ul>
-            <ul className="grid grid-cols-2 gap-1 p-7 w-[70%] text-xl bg-[#EAFBF9] rounded-2xl    text-[#003F37] font-bold text-center ">
-              <li className="flex  m-2 justify-center flex-col w-[50%] ">
-                <img src={php} alt="" className="w-full " />
-                Map Api
+            <ul className="flex  gap-1 p-7 w-[80%] [1.2rem]  bg-[#EAFBF9] rounded-2xl    text-[#003F37] font-bold text-center ">
+             
+            <li className="">
+                <img src={php} alt="" className="w-full p-5 py-2 " />
+                Anroid              </li>
+              <li>
+                <img src={php} alt="" className="w-full p-5 py-2 " />
+              ios
               </li>
             </ul>
           </div>
