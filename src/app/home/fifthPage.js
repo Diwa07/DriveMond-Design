3e5e90a995e63f85b65d7073bf037fc1e00f3adb
Diwakar <khatiwadaddiwakar07@gmail.com>
2024-02-27 12:38:21 +0545

    solved4

--- src/app/home/fifthPage.js
+++ src/app/home/fifthPage.js
@@ -4,8 +4,8 @@ import React from 'react'
  import { TbTruckDelivery } from "react-icons/tb";
 const FifthPage = () => {
   return (
-    <div className='h-screen mt-6 ' >
-    <div className="flex flex-col items-center h-screen border border-black "style={{ backgroundImage: `url(${bg})` }}>
+    <div className='h-auto mt-6 md:h-screen' >
+    <div className="flex flex-col items-center border border-black md:h-screen "style={{ backgroundImage: `url(${bg})` }}>
 <div className='mt-[4rem] px-5  text-white text-center  text-2xl' >
    <h1 className='font-bold text-[2rem] ' >
    Let’s Discover DriveMond’s Business Potential
@@ -14,7 +14,7 @@ const FifthPage = () => {
    With DriveMond’s two dynamic modules, you can do various businesses and unlock the utmost potential.
    </h2>
 </div>
-<div className='grid mt-9   md:px-[4rem] gap-11 items-center    h-[20rem] grid-cols-1 md:grid-cols-2' >
+<div className='grid mt-9 px-5  md:px-[4rem] gap-11 items-center    h-[20rem] grid-cols-1 md:grid-cols-2' >
   <div  className='px-9 rounded-2xl flex flex-col justify-between items-center  h-[20rem] py-7 w-[25rem] bg-[#E9FFFC] ' >
   <FaCar  className='text-[7rem] text-[#00A08D] ' />
   <h1 className='text-2xl font-bold text-[#00423A]' >Ride Sharing</h1>
