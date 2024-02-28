8df72d7e4ff6de2b1f74368cb302ba96d51aeb23
Diwakar <khatiwadaddiwakar07@gmail.com>
2024-02-28 23:00:11 +0545

    solved6

--- src/app/home/ninthPage.js
+++ src/app/home/ninthPage.js
@@ -16,19 +16,19 @@ const NinthPage = () => {
       </div>
 
       <div className='grid  text-[#FFFFFF] text-xl font-bold h-auto grid-cols-1 px-3 py-10 gap-11 md:gap-5 md:grid-cols-3'> 
-        <figure className='flex h-auto border hover:border-[#00A08D] px-1 flex-col justify-center sm:h-[23rem] w-[10rem] sm:w-[17rem] rounded-2xl'>
+        <figure className='flex h-auto border hover:border-[#00A08D] px-1 flex-col justify-center md:h-[23rem] w-[15rem] md:w-[17rem] rounded-2xl'>
           <div className='flex'>
             <img src={support} alt='Support' className='' />
           </div>
           <figcaption className='text-center bg-[#006156] rounded-xl p-4 mt-5 '>Support</figcaption>
         </figure>
 
-        <figure className='flex h-auto border hover:border-[#00A08D] px-1 flex-col justify-center sm:h-[23rem] w-[10rem] sm:w-[17rem] rounded-2xl'>
+        <figure className='flex h-auto border hover:border-[#00A08D] px-1 flex-col justify-center md:h-[23rem] w-[15rem] md:w-[17rem] rounded-2xl'>
           <img src={faqs} alt='FAQs' />
           <figcaption className='text-center bg-[#006156] rounded-xl p-4 mt-5 '>FAQs</figcaption>
         </figure>
 
-        <figure className='flex h-auto flex-col border px-1 hover:border-[#00A08D] justify-center sm:h-[23rem] w-[10rem] sm:w-[17rem] rounded-2xl'>
+        <figure className='flex h-auto flex-col border px-1 hover:border-[#00A08D] justify-center md:h-[23rem] w-[15rem] md:w-[17rem] rounded-2xl'>
           <img src={changelog} alt='Changelog' />
           <figcaption className='text-center bg-[#006156] rounded-xl p-4 mt-5 '> Changelog</figcaption>
         </figure>
